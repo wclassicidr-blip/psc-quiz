1a878cd84f05277f991bfb07de36b1808175be5d
wclassicidr-blip <wclassicidr@gmail.com>
2025-10-15 19:07:27 +0700

    Delete pages directory

--- pages/index.js
+++
@@ -1,5 +0,0 @@
-// pages/index.js
-export default function Home() { return null; }
-export async function getServerSideProps() {
-  return { redirect: { destination: '/index.html', permanent: false } };
-}
